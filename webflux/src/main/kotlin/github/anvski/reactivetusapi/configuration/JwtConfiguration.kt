13fb176052ffdc0ba868e413d5be0bb6c3425b2b
andrej.veljanovski <andrej.veljanovski10099@gmail.com>
2024-03-15 16:28:33 +0100

    empty directories for convenient project start, key update

--- webflux/src/main/kotlin/github/anvski/reactivetusapi/configuration/JwtConfiguration.kt
+++ webflux/src/main/kotlin/github/anvski/reactivetusapi/configuration/JwtConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class JwtConfiguration {
-    private val authenticationSecretKey = "=====anvski======"
+    private val authenticationSecretKey = "=====anvski============================"
 
     private val validityInMs: Long = 8640000000
 
